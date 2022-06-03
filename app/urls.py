c02203cbd5f2021b4a591c4fbd851979ac39091b
IanoSang <ianosang18@gmail.com>
2022-06-04 02:40:29 +0300

    feat: urls- adds a routing to the urs patterns

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
