b4a8837a1b12ed281c04b82084a42a0535da56e8
IanoSang <ianosang18@gmail.com>
2022-06-04 17:31:35 +0300

    feat: views- adds profiles and users profiles view funtions

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import SignUpForm, UpdateUserProfileForm, UpdateUserForm
+from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Follow
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
 
@@ -21,3 +26,49 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+
+@login_required(login_url='login')
+def profile(request, username):
+    images = request.user.profile.posts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form,
+        'images': images,
+
+    }
+    return render(request, 'profile.html', params)
+
+
+@login_required(login_url='login')
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_prof.profile.posts.all()
+
+    followers = Follow.objects.filter(followed=user_prof.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            follow_status = True
+        else:
+            follow_status = False
+    params = {
+        'user_prof': user_prof,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    print(followers)
+    return render(request, 'user_profile.html', params)
