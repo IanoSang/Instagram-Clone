3f866658e35c2111e923615944feed9fc4926013
IanoSang <ianosang18@gmail.com>
2022-06-09 00:38:22 +0300

    feat: models- replaces the images fields with cloudinary

--- app/models.py
+++ app/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_pic = models.ImageField(upload_to='images/', default='default.png')
+    profile_pic = CloudinaryField(default='https://res.cloudinary.com/dpww3jwgm/image/upload/v1654722449/default.png')
     bio = models.TextField(max_length=500, default="My Bio", blank=True)
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
@@ -36,7 +37,7 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
-    image = models.ImageField(upload_to='posts/')
+    image = CloudinaryField()
     name = models.CharField(max_length=250, blank=True)
     caption = models.CharField(max_length=250, blank=True)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
@@ -67,7 +68,7 @@ class Post(models.Model):
 
 class Likes(models.Model):
     like = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes', blank=True, null=True)
-    unlike = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='dislikes',  null=True)
+    unlike = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='dislikes', null=True)
 
     def __str__(self):
         return f'{self.like} Likes'
