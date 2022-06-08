9a5925fb65c31be4e25c8aca29cc543451f4e7ec
IanoSang <ianosang18@gmail.com>
2022-06-08 23:02:33 +0300

    feat: admin- registers the likes class model to the admin page

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Post, Comment, Follow
+from .models import Profile, Post, Comment, Follow, Likes
 
 admin.site.register(Profile),
 admin.site.register(Post),
 admin.site.register(Comment),
 admin.site.register(Follow),
+admin.site.register(Likes),
