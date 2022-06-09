d97605c40795482bd611ebdc243bee244a53b5a4
IanoSang <ianosang18@gmail.com>
2022-06-09 17:25:57 +0300

    feat: admin- deregisters likje model

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Post, Comment, Follow, Likes
+from .models import Profile, Post, Comment, Follow
 
 admin.site.register(Profile),
 admin.site.register(Post),
 admin.site.register(Comment),
 admin.site.register(Follow),
-admin.site.register(Likes),
+
